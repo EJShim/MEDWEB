f35e7cca3675d0ebb719b63fedea2ab006533676
ejshim <soulrommel@gmail.com>
2016-11-15 00:34:55 +0900

    port changed

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static('public'));
 
-var server = app.listen(80, function(){
+var server = app.listen(8080, function(){
     console.log("Express server has started on port 80")
 });
